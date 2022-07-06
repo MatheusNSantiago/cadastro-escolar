15489c305992568c3038d67f3c5cb47d28401796
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-07-05 21:42:28 -0300

    leitura parecida com a do python, mas com max len

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -15,28 +15,37 @@ bool escolher(char *m0, char *m1, char *m2)
 
     if (opcao == 1)
         return true;
-    
+
     return false;
 }
 
 void ler(char *input, int len)
 {
+    int user_input_len;
     do
     {
-        if (fgets(input, len, stdin))
+        if (fgets(input, len, stdin) != 0)
+        {
+            user_input_len = strlen(input);
 
-            if (sscanf(input, "%[^\n]s", input) == 1)
+            if (user_input_len > 0 && input[user_input_len - 1] != '\n')
+            {
+                char c;
+                while ((c = getchar()) != '\n' && c != EOF)
+                    user_input_len++;
+            }
+            if (user_input_len > len - 1)
+            {
+                printf("Input deve conter no máximo %d caracteres.\nDigite novamente: ", len - 1);
+                sleep(SLEEP);
+            }
+            else
+            {
+                input[strcspn(input, "\n")] = 0;
                 return;
-        printf("Input inválido. Digite novamente: ");
+            }
+        }
+        else
+            puts("Input invalido. Digite novamente: ");
     } while (true);
-}
-
-void extract_arguments_from_string(char **dest, char *str)
-{
-    char *token = strtok(str, ";");
-    for (int arg_count = 0; token != NULL; arg_count++)
-    {
-        dest[arg_count] = token;
-        token = strtok(NULL, ";");
-    }
 }
